f1fdca952d3bb7666e82fcdb2e457b32ef791984
wandersonsantiago <wandersonsantiago@wandersonsantiago>
2017-03-18 13:37:36 -0300

    inicial

---
+++ src/main/java/br/com/blog/BlogProgramadorApplication.java
@@ -0,0 +1,12 @@
+package br.com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProgramadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProgramadorApplication.class, args);
+	}
+}
